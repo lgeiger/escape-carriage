99da9f9ebe69e35bce919a551691b1d8a37f32c5
Lukas Geiger <lukas.geiger94@gmail.com>
2016-10-13 18:47:05 +0200

    Use var instead of let

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function escapeCarriageReturn(txt) {
   txt = txt.replace(/\r+\n/gm, '\n'); // \r followed by \n --> newline
   while (txt.search(/\r/g) > -1) {
-    let base = txt.match(/^.*\r+/m)[0].replace(/\r/, '');
-    let insert = txt.match(/\r+.*$/m)[0].replace(/\r/, '');
+    var base = txt.match(/^.*\r+/m)[0].replace(/\r/, '');
+    var insert = txt.match(/\r+.*$/m)[0].replace(/\r/, '');
     insert = insert + base.slice(insert.length, base.length);
 
     txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r+/m, insert);
