175c52484417647659fdfcdc129d6ea5f753555b
Lukas Geiger <lukas.geiger94@gmail.com>
2016-12-16 17:18:38 +0100

    Fix carrigage return handling
    https://github.com/jupyter/notebook/pull/1982

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 function escapeCarriageReturn(txt) {
   txt = txt.replace(/\r+\n/gm, '\n'); // \r followed by \n --> newline
-  while (txt.search(/\r/g) > -1) {
-    var base = txt.match(/^.*\r+/m)[0].replace(/\r/, '');
-    var insert = txt.match(/\r+.*$/m)[0].replace(/\r/, '');
-    insert = insert + base.slice(insert.length, base.length);
-
-    txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r+/m, insert);
+  while (txt.search(/\r[^$]/g) > -1) {
+      var base = txt.match(/^(.*)\r+/m)[1];
+      var insert = txt.match(/\r+(.*)$/m)[1];
+      insert = insert + base.slice(insert.length, base.length);
+      txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r/m, insert);
   }
   return txt;
 }
