31fc90704da852657ee13b81e7fc251d15bc73e0
Lukas Geiger <lukas.geiger94@gmail.com>
2017-05-10 17:09:15 +0200

    Improve performance for normal text

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function escapeCarriageReturn(txt) {
-  if(!txt) return '';
+  if (!txt) return '';
+  if (!/\r/.test(txt)) return txt;
   txt = txt.replace(/\r+\n/gm, '\n'); // \r followed by \n --> newline
-  while (txt.search(/\r[^$]/g) > -1) {
-      var base = txt.match(/^(.*)\r+/m)[1];
-      var insert = txt.match(/\r+(.*)$/m)[1];
-      insert = insert + base.slice(insert.length, base.length);
-      txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r/m, insert);
+  while (/\r[^$]/.test(txt)) {
+    var base = /^(.*)\r+/m.exec(txt)[1];
+    var insert = /\r+(.*)$/m.exec(txt)[1];
+    insert = insert + base.slice(insert.length, base.length);
+    txt = txt.replace(/\r+.*$/m, '\r').replace(/^.*\r/m, insert);
   }
   return txt;
 }
